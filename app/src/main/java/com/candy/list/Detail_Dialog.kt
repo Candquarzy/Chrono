3d67e20aa0d0c4cd2a209cd8e704d5a3585c28ed
CandyQuartz_QwQ <ftm948794kuang@outlook.com>
2025-08-17 22:09:39 +0800

    update font design

--- app/src/main/java/com/candy/list/Detail_Dialog.kt
+++ app/src/main/java/com/candy/list/Detail_Dialog.kt
@@ -1,37 +1,32 @@
 package com.candy.list
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.material3.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material3.FilledTonalIconButton
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import com.candy.list.data.Card_Data
 
 
@@ -85,6 +80,11 @@ fun Detail(show:() -> Unit, data: Card_Data)
 								fontWeight = FontWeight.ExtraBold,
 								fontSize = 28.sp,
 								color = data.color,
+								style = TextStyle(
+									shadow = Shadow(
+										color = Color.Gray, offset = Offset(3f, 3f), blurRadius = 3f
+									)
+								)
 							)
 						}
 					}
