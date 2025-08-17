80810956ac0a9c7c46fa2701dca0be2a0dbb00d6
CandyQuartz_QwQ <ftm948794kuang@outlook.com>
2025-08-17 17:36:48 +0800

    update tips

--- app/src/main/java/com/candy/list/MainActivity.kt
+++ app/src/main/java/com/candy/list/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.candy.list
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +18,7 @@ import com.candy.list.ui.theme.ListTheme
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity()
 {
+	@RequiresApi(Build.VERSION_CODES.O)
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
--- app/src/main/java/com/candy/list/Main_View.kt
+++ app/src/main/java/com/candy/list/Main_View.kt
@@ -2,28 +2,20 @@ package com.candy.list
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.White
 import com.candy.list.data.Data.card_data
 
 
@@ -56,7 +48,7 @@ fun Main_View(innerPadding: PaddingValues)
 				)
 				{
 					Text(
-						text = "这是我的记录",
+						text = "这是我的记录", //h1 title 大标题
 						fontSize = 32.sp,
 						fontWeight = FontWeight.ExtraBold,
 						color = Color.Black,
